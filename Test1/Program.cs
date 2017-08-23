5a0ff50e89d5347cbdc8f1190cdbe956c9c266ae
taufikprabowo1121 <solidiercyber@gmail.com>
2017-08-23 16:12:12 +0700

    jhon

---
+++ Test1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nama;
+            string alamat;
+            int umur;
+            string golongan;
+
+            Console.Write("Masukan nama : ");
+            nama = Console.ReadLine();
+            Console.Write("Masukan Alamat : ");
+            alamat = Console.ReadLine();
+            Console.Write("Masukan Umur : ");
+            umur = int.Parse(Console.ReadLine());
+            Console.Write("Masukan Golongan Darah : ");
+            golongan = Console.ReadLine();
+
+
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Nama Anda: " + nama);
+            Console.WriteLine("Alamat Anda: " + alamat);
+            Console.WriteLine("Umur Anda: " + umur);
+            Console.WriteLine("Golongan Darah: " + golongan);
+            Console.WriteLine("-------------------------------");
+
+            Console.ReadKey();
+        }
+    }
+}
